ad03064cfcde86e39bdcfeca3bf02cd4cb4e2ee7
Siddharth <siddharthpatilgurav@gmail.com>
2025-08-23 00:13:49 +0530

    fix:cors error

--- api/src/lib/utils.ts
+++ api/src/lib/utils.ts
@@ -3,8 +3,7 @@ import crypto from "crypto";
 
 export const corsOptions: CorsOptions = {
     origin: [
-        "http://localhost:3000",
-        process.env.CLIENT_URL || 'http://localhost:3000',
+        "http://localhost:3001",
     ],
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true
